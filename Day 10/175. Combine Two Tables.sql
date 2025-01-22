e365b4dcad24f116300b8cdde8d39c419947b69e
Tanuja <tanuja.narravula142327@gmail.com>
2025-01-22 15:02:18 -0600

    Day 10

---
+++ Day 10/175. Combine Two Tables.sql
@@ -0,0 +1,9 @@
+-- Write your PostgreSQL query statement below
+SELECT 
+    person.firstName,
+    person.lastName,
+    address.city,
+    address.state
+FROM person
+LEFT JOIN address 
+    ON person.personId = address.personId
