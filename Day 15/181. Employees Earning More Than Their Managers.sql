d250d1a33861ce85683f71548c7b1c00baf42e52
Tanuja <tanuja.narravula142327@gmail.com>
2025-01-24 16:03:43 -0600

    Day 15

---
+++ Day 15/181. Employees Earning More Than Their Managers.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+SELECT 
+    e1.name AS "Employee"
+FROM employee AS e1
+JOIN employee AS e2 
+    ON e1.managerId = e2.id
+WHERE e1.salary > e2.salary
